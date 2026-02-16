bb40706b51559fabc2f7565d0fdf97a4a32b38d8
tnmoy-paul01 <tnmypl2003@gmail.com>
2026-02-16 16:01:46 +0530

    remove captcha

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,14 @@ export default function Contact() {
     const [result, setResult] = useState("");
     const onSubmit = async (event) => {
         event.preventDefault();
-        const hCaptcha = event.target.querySelector('textarea[name=h-captcha-response]').value;
-        if (!hCaptcha) {
-            event.preventDefault();
-            setResult("Please fill out captcha field");
-            return
-        }
+
+        // const hCaptcha = event.target.querySelector('textarea[name=h-captcha-response]').value;
+        // if (!hCaptcha) {
+        //     event.preventDefault();
+        //     setResult("Please fill out captcha field");
+        //     return
+        // }
+
         setResult("Sending....");
         const formData = new FormData(event.target);
 
@@ -32,47 +34,48 @@ export default function Contact() {
         }
     };
 
-    function CaptchaLoader() {
-        const captchadiv = document.querySelectorAll('[data-captcha="true"]');
-        if (captchadiv.length) {
-            let lang = null;
-            let onload = null;
-            let render = null;
+    // function CaptchaLoader() {
+    //     const captchadiv = document.querySelectorAll('[data-captcha="true"]');
+    //     if (captchadiv.length) {
+    //         let lang = null;
+    //         let onload = null;
+    //         let render = null;
 
-            captchadiv.forEach(function (item) {
-                const sitekey = item.dataset.sitekey;
-                lang = item.dataset.lang;
-                onload = item.dataset.onload;
-                render = item.dataset.render;
+    //         captchadiv.forEach(function (item) {
+    //             const sitekey = item.dataset.sitekey;
+    //             lang = item.dataset.lang;
+    //             onload = item.dataset.onload;
+    //             render = item.dataset.render;
 
-                if (!sitekey) {
-                    item.dataset.sitekey = "50b2fe65-b00b-4b9e-ad62-3ba471098be2";
-                }
-            });
+    //             if (!sitekey) {
+    //                 item.dataset.sitekey = "50b2fe65-b00b-4b9e-ad62-3ba471098be2";
+    //             }
+    //         });
 
-            let scriptSrc = "https://js.hcaptcha.com/1/api.js?recaptchacompat=off";
-            if (lang) {
-                scriptSrc += `&hl=${lang}`;
-            }
-            if (onload) {
-                scriptSrc += `&onload=${onload}`;
-            }
-            if (render) {
-                scriptSrc += `&render=${render}`;
-            }
+    //         let scriptSrc = "https://js.hcaptcha.com/1/api.js?recaptchacompat=off";
+    //         if (lang) {
+    //             scriptSrc += `&hl=${lang}`;
+    //         }
+    //         if (onload) {
+    //             scriptSrc += `&onload=${onload}`;
+    //         }
+    //         if (render) {
+    //             scriptSrc += `&render=${render}`;
+    //         }
 
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.async = true;
-            script.defer = true;
-            script.src = scriptSrc;
-            document.body.appendChild(script);
-        }
-    }
+    //         var script = document.createElement("script");
+    //         script.type = "text/javascript";
+    //         script.async = true;
+    //         script.defer = true;
+    //         script.src = scriptSrc;
+    //         document.body.appendChild(script);
+    //     }
+    // }
 
-    useEffect(() => {
-        CaptchaLoader();
-    }, []);
+    // useEffect(() => {
+    //     CaptchaLoader();
+    // }, []);
+    
     return (
         <div id="contact" className="w-full px-[12%] py-10 scroll-mt-10 bg-[url('./assets/footer-bg-color.png')] bg-no-repeat bg-[length:90%_auto] bg-center dark:bg-none">
 
@@ -82,14 +85,14 @@ export default function Contact() {
 
             <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
 
-                <input type="hidden" name="subject" value="Tanmoy Paul - New form Submission" />
+                <input type="hidden" name="subject" value="Tanmoy Paul - New form Submission from portfolio" />
 
                 <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
                     <input type="text" placeholder="Enter your name" className="flex-1 px-3 py-2 focus:ring-1 outline-none border border-blue-500/60 dark:border-green-500/20 rounded-sm bg-white dark:bg-black" required name="name" />
                     <input type="email" placeholder="Enter your email" className="flex-1 px-3 py-2 focus:ring-1 outline-none border border-blue-500/60  dark:border-green-500/20 rounded-sm bg-white dark:bg-black" required name="email" />
                 </div>
                 <textarea rows="6" placeholder="Enter your message" className="w-full px-4 py-2 focus:ring-1 outline-none border border-blue-500/60 dark:border-green-500/20 rounded-sm bg-white mb-6 dark:bg-black" required name="message"></textarea>
-                <div className="h-captcha mb-6 max-w-full" data-captcha="true"></div>
+                {/* <div className="h-captcha mb-6 max-w-full" data-captcha="true"></div> */}
                 <button type='submit' className="border border-blue-600 bg-blue-500/10 hover:bg-blue-500/20 py-2 px-8 w-max flex items-center justify-between gap-2 rounded-sm mx-auto hover:bg-black duration-500 dark:text-black dark:border dark:border-[#1EFF00] dark:hover:border-[#15B100] dark:bg-[#1EFF00] dark:hover:bg-[#15B100] transition">
                 Submit now
                     <img src="./assets/arrow-icon.png" alt="" className="w-3" />
